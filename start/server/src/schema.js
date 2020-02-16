65841c291f6fb047d9ad721ea300bc329810e517
hiroya.shimamoto <shshimamo@gmail.com>
2020-02-16 17:48:32 +0900

    Write resolvers on types

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -23,7 +23,7 @@ const typeDefs = gql`
 
   type Mission {
     name: String
-    missionPatch(size: PatchSize): String
+    missionPatch(mission: String, size: PatchSize): String
   }
 
   enum PatchSize {
