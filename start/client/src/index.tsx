4c2ca6e64e654004332449d9648db0920d014f95
hiroya.shimamoto <shshimamo@gmail.com>
2020-02-18 03:54:05 +0900

    Add authorization header to ApolloClient

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -8,6 +8,26 @@ import ReactDOM from 'react-dom';
 import Pages from './pages';
 import injectStyles from './styles';
 
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+
+    headers: {
+      authorization: localStorage.getItem('token'),
+    }, 
+  }),
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  },
+});
+
+
 // previous variable declarations
 
 
